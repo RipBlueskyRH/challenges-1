4e78f4dbbd74852e97ac5cdab15c474e46e32ee4
Rip_Blue <ripblueskyrh@gmail.com>
2024-12-21 09:11:59 -0500

    Add files via upload

---
+++ averagespeed.py
@@ -0,0 +1,9 @@
+a = int (input ("enter a number of mph:"))
+b = int (input ("enter another number of mph: "))
+c = int (input ("enter another number of mph: "))
+d = int (input ("enter another number of mph: "))
+e = int (input ("enter one more number of mph: "))
+num=a+b+c+d+e
+average=num/5
+print ("average speed is" ,average)
+
---
+++ divisible.py
@@ -0,0 +1,6 @@
+z=int (input ("enter a dividend"))
+a=int (input ("enter a divisor"))
+if z%a==0:
+    print ("the dividend is divisible by the divisor 🐶")
+else:
+    print ("the dividend is not divisible by the divisor 🐶")
